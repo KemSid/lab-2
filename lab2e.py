035313af029f3be257509e013b6f73bc40442463
KemSid <sid.p.slick@gmail.com>
2024-10-29 19:45:01 -0400

    Add files via upload

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Sidney Mills
+# Author ID: KemSid
+# Date Created: 2024/10/22
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
